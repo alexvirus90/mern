75596bbd721f6374292e9dba1b43b8362c5646a5
Alexandr Komarov <alexandr.komarov@veeam.com>
2021-08-15 18:30:44 +0700

    Добавлена валидация при регистрации

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,8 +5,23 @@ const User = require('../models/User')
 const router = Router()
 
 // /api/auth/register
-router.post('/register', async (req, res) => {
+router.post(
+    '/register',
+    [
+        check('email', 'Некорректный email').isEmail(),
+        check('password', 'Минимальная длина пароля 6 символов').isLength({min: 6})
+    ],
+    async (req, res) => {
     try {
+        const errors = validationResult(req)
+
+        if (errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'Некорректные данные при регистрации!'
+            })
+        }
+
         const {email, password} = req.body
         const candidate = await User.findOne({email})
 
