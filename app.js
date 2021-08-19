b27fa400e5661b751bac77d884ff6af78847f512
Alexandr Komarov <alexandr.komarov@veeam.com>
2021-08-19 20:25:18 +0700

    Добавлена регистрация.

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-const express = require('express')
-const config = require('config')
-const mongoose = require('mongoose')
+const express = require("express");
+const config = require("config");
+const path = require("path");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
 
-app.use('/api/auth', require('./routes/auth.routes'))
+app.use(express.json({ extended: true }));
 
-const PORT = config.get('port') || 5000
+app.use("/api/auth", require("./routes/auth.routes"));
 
-async function start(){
-    try {
-        await mongoose.connect(config.get('mongoUri'), {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        })
-        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
-    } catch (e) {
-        console.log('Server Error', e.message)
-        process.exit(1)
-    }
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
-start()
+const PORT = config.get("port") || 5000;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUri"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    app.listen(PORT, () =>
+      console.log(`App has been started on port ${PORT}...`)
+    );
+  } catch (e) {
+    console.log("Server Error", e.message);
+    process.exit(1);
+  }
+}
+
+start();
